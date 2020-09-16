263df7aba41d7e85d13e104f892c733e39305d5c
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-09-17 07:17:46 +0800

    Temp change

--- src/Modules/Shelf/ProductList/Product/Product.js
+++ src/Modules/Shelf/ProductList/Product/Product.js
@@ -2,48 +2,60 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { isUndefined } from 'lodash';
 
 // import local files
 import style from './index.module.scss';
+import util from '../../../../Utilities/util.js';
 
 //state management
 import {
 	addToCart,
 	updateCart,
-	cartList,
+	cart,
+	cartIsOpen,
 } from '../../../../Ducks/Features/CartSlice.js';
 
 export default function Product({ products }) {
-	const [quantity, setQuantity] = useState(1);
-	const productId = products.id;
-	const productsArray = useSelector((state) => cartList.selectAll(state));
-
-	const addProductToCart = (item) => {
-		const addItemToCart = {
-			...item,
-			id: item.id,
-			quantity,
-		};
-		let idAlreadyExists = productsArray.find(
-			(product) => product.id == productId
-		);
-
-		if (!idAlreadyExists) dispatch(addToCart(addItemToCart));
-		setQuantity(quantity + 1);
-		dispatch(
-			updateCart({
-				id: addItemToCart.id,
-				changes: { quantity },
-			})
-		);
-	};
+	const { id } = products;
+	const isOpen = useSelector((state) => state.cart.isOpen);
+	const cartProductsList = useSelector((state) => cart.selectAll(state));
+	const getProductOnCart = useSelector((state) => cart.selectById(state, id));
 
 	const dispatch = useDispatch();
+
+	const addItemToCart = () => {
+		let isQty = isUndefined(getProductOnCart);
+		let quantity = 1;
+		let newItemProduct = {
+			...products,
+			quantity,
+			total: products.price,
+		};
+		dispatch(cartIsOpen(!isOpen));
+
+		// check if product already exist in the cart.
+		let idAlreadyExists = cartProductsList.find((product) => product.id == id);
+		// otherwise add new product in the cart
+		if (!idAlreadyExists) dispatch(addToCart(newItemProduct));
+		// if exist and not undefined, update quantity
+		if (!isQty) {
+			dispatch(
+				updateCart({
+					id: newItemProduct.id,
+					changes: {
+						quantity: getProductOnCart.quantity + 1,
+						total: getProductOnCart.quantity * products.price + products.price,
+					},
+				})
+			);
+		}
+	};
 
 	return (
 		<div
 			className={`col rounded ${style.col}`}
-			key={Math.random()}
+			key={id}
 			data-sku={products.sku}>
 			<div className='card p-4'>
 				<img
@@ -56,11 +68,11 @@ export default function Product({ products }) {
 				</div>
 				<button
 					type='button'
-					className='btn btn-dark'
+					className='btn btn-outline-secondary'
 					onClick={() => {
-						addProductToCart(products);
+						addItemToCart(products);
 					}}>
-					Add to cart <span>{productId}</span>
+					Add to cart
 				</button>
 			</div>
 		</div>
@@ -75,6 +87,7 @@ Product.propTypes = {
 		name: PropTypes.string,
 		price: PropTypes.number,
 		published: PropTypes.number,
+		quantity: PropTypes.number,
 		sku: PropTypes.string,
 	}),
 };
