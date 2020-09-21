2c9e43df0cf1316db7873f2f5a38d6d7bf00ef36
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-09-21 13:45:19 +0800

    Update search component

--- src/Modules/Shelf/SearchProduct/SearchProduct.js
+++ src/Modules/Shelf/SearchProduct/SearchProduct.js
@@ -3,14 +3,15 @@ import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
 // import local file
+import style from './index.module.scss';
 import Header from '../../../Components/Header';
 import { getProductQuery } from '../../../Ducks/Features/SearchSlice';
 export default function SearchProduct() {
 	const dispatch = useDispatch();
 	return (
 		<Header strong='Menu' regular='Category'>
-			<div className='input-group mb-3'>
-				<span className='input-group-text bg-transparent'>&#128270;</span>
+			<div className={`input-group mb-3 ${style.search}`}>
+				<span className='input-group-text '>&#128270;</span>
 				<input
 					type='text'
 					className='form-control'
