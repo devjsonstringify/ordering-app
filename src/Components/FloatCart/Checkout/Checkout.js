5e9eba26b3007ac5cf5e1ac98496fb8e7f007903
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-09-24 17:52:47 +0800

    Feature: Add subtotal, tax calculation and grand total on cart

--- src/Components/FloatCart/Checkout/Checkout.js
+++ src/Components/FloatCart/Checkout/Checkout.js
@@ -1,23 +1,44 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 //import local file
 import style from './index.module.scss';
 import Total from './Total.js';
 import Button from '../../Button';
+import { cart } from '../../../Ducks/Features/CartSlice.js';
 
 export default function Checkout() {
+	const productsOnCart = useSelector((state) => cart.selectAll(state));
+
+	const calculateTotal = productsOnCart.reduce(function (prev, cur) {
+		return prev + cur.total;
+	}, 0);
+
+	const tax = 0.025;
+	let subTotal = calculateTotal;
+	const calculateTax = subTotal * tax;
+	const grandTotal = calculateTax + subTotal;
+
 	return (
 		<div className={`mt-5 h-25 p-4 ${style.checkout}`}>
-			<div className='cart-taxes'>
+			<div className='cart-taxes '>
 				<table className='table table-borderless'>
 					<tbody>
 						<tr>
-							<td>Sub Total</td>
-							<td>$22.00</td>
+							<th>Desciption</th>
+							<th>Amount</th>
 						</tr>
 						<tr>
-							<th>PB (5%)</th>
-							<td>$2.12</td>
+							<th>Sub Total</th>
+							<th>${subTotal.toFixed(2)}</th>
+						</tr>
+						<tr>
+							<th>Tax rate</th>
+							<td>$2.5%</td>
+						</tr>
+						<tr>
+							<th>Total Tax</th>
+							<td>${calculateTax.toFixed(2)}</td>
 						</tr>
 					</tbody>
 				</table>
@@ -26,9 +47,7 @@ export default function Checkout() {
 				<Button style='w-100' handleClick={() => console.log('checkout...')}>
 					<h5>
 						Charge
-						<span>
-							<Total />
-						</span>
+						<span className='mx-3'>${grandTotal.toFixed(2)}</span>
 					</h5>
 				</Button>
 			</div>
