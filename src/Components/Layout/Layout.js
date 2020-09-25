e761646f5eda00e651ae3b3b79995e08e6b9f321
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-09-25 14:54:00 +0800

    Removed: code clean up remove unused code

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,17 +1,13 @@
 // import react library
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 //import local files
-import style from './index.module.scss';
 import Navigation from '../Navigation/Navigation';
 import Main from '../Main/Main';
-import Sidebar from '../Sidebar';
-import FloatCart from '../FloatCart';
+import style from './index.module.scss';
 
 // state management
-import { cart } from '../../Ducks/Features/CartSlice.js';
 import SideBar from '../Sidebar';
 
 export default function Layout({ children }) {
