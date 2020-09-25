2aa8d2cc2f1167af24b704be7618688b3282ade2
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-09-25 14:07:16 +0800

    Feature: Toggle float

--- src/Components/Sidebar/SideBar.js
+++ src/Components/Sidebar/SideBar.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 //import local files
-import style from './index.module.scss';
 import FloatCart from '../FloatCart';
 import Checkout from '../FloatCart/Checkout';
 import CartHeader from '../FloatCart/CartHeader';
+
+import './style.scss';
 
 // state management
 import { cart } from '../../Ducks/Features/CartSlice.js';
@@ -13,11 +14,16 @@ import EmptyCart from '../FloatCart/EmptyCart';
 
 export default function SideBar() {
 	//local state
-	const [isOpen, setOpen] = useState(false);
+	const isOpen = useSelector((state) => state.cart.isOpen);
 	const productsOnCart = useSelector((state) => cart.selectAll(state));
 
+	let classes = ['sidebar'];
+	if (isOpen) {
+		classes.push('sidebar__open');
+	}
+
 	return (
-		<div className={`col-3 container h-auto pt-5 ${style.sideBar}`}>
+		<div className={classes.join(' ')}>
 			{productsOnCart.length > 0 ? (
 				<>
 					<CartHeader {...productsOnCart} />
@@ -27,7 +33,6 @@ export default function SideBar() {
 			) : (
 				<EmptyCart />
 			)}
-			{}
 		</div>
 	);
 }
