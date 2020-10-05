b7e8ea445bd03bca7309027de3c5ba74d65af456
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-10-05 17:09:09 +0800

    Update and fixed: float cart, make delete icon by default, no need toggle button to edit cart and add bit styling

--- src/Components/FloatCart/Cart/CartProduct/CartProduct.js
+++ src/Components/FloatCart/Cart/CartProduct/CartProduct.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
 // import files
-import style from './index.module.scss';
+import './style.scss';
 import {
 	cart,
 	updateCart,
@@ -50,56 +50,46 @@ export default function Product({ product }) {
 	};
 
 	return (
-		<div className={`cart-item pb-1 ${style.product}`}>
-			{isEdit ? (
-				<button
-					type='button'
-					className='btn btn-link p-0'
-					onClick={() => dispatch(deleteCart(id))}>
-					<svg
-						width='1em'
-						height='1em'
-						viewBox='0 0 16 16'
-						className='bi bi-trash'
-						fill='#ff2351'
-						xmlns='http://www.w3.org/2000/svg'>
-						<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z' />
-						<path
-							fillRule='evenodd'
-							d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'
-						/>
-					</svg>
-				</button>
-			) : null}
-			<div
-				className={`${
-					isEdit
-						? 'cart-item-details container d-flex align-items-baseline mx-2'
-						: 'cart-item-details container d-flex align-items-baseline'
-				}`}>
+		<div className='cart-item pb-1 product'>
+			<div className='cart-item-details'>
 				<Card
-					rowPos='flex-fill d-flex justify-content-evenly'
+					rowPos='flex-fill d-flex '
 					shape='square'
 					size='xsmall'
 					thumbnail={require(`../../../../Assets/Products/${sku}.png`)}
 					{...product}
 				/>
-				<Quantity
-					handleDecrementQuantity={() =>
-						handleDecrementQuantity(product, cartId)
-					}
-					handleIncrementQuantity={() =>
-						handleIncrementQuantity(product, cartId)
-					}
-					minimumProduct={minimumProduct}
-					quantity={quantity}
-					classes={`${
-						isEdit
-							? 'd-flex justify-content-around align-items-center mx-4'
-							: 'flex-fill'
-					}`}
-				/>
-				<QuantityTotal classes='flex-fill' amount={calculateAmount} />
+				<div className='d-flex align-items-baseline'>
+					<QuantityTotal classes='col' amount={calculateAmount} />
+					<Quantity
+						handleDecrementQuantity={() =>
+							handleDecrementQuantity(product, cartId)
+						}
+						handleIncrementQuantity={() =>
+							handleIncrementQuantity(product, cartId)
+						}
+						minimumProduct={minimumProduct}
+						quantity={quantity}
+						classes='d-flex justify-content-around align-items-center col flex-grow-1 '
+					/>
+					<button
+						type='button'
+						className='btn btn-link p-0 col-1'
+						onClick={() => dispatch(deleteCart(id))}>
+						<svg
+							width='1.25em'
+							height='1.25em'
+							viewBox='0 0 16 16'
+							className='bi bi-trash'
+							fill='#ff2351'
+							xmlns='http://www.w3.org/2000/svg'>
+							<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'></path>
+							<path
+								fillRule='evenodd'
+								d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'></path>
+						</svg>
+					</button>
+				</div>
 			</div>
 		</div>
 	);
--- src/Components/FloatCart/Cart/Quantiy/Quantity.js
+++ src/Components/FloatCart/Cart/Quantiy/Quantity.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 // import local file
 import Button from '../../../Button';
-
 export default function Quantity({
 	classes,
 	minimumProduct,
@@ -14,24 +12,20 @@ export default function Quantity({
 }) {
 	let style = ['quantity'];
 	style.push(classes);
-	const editCart = useSelector((state) => state.cart.isEdit);
 	return (
 		<div className={style.join(' ')}>
-			{editCart ? (
-				<Button
-					style='btn-light mx-1 '
-					disabled={minimumProduct}
-					handleClick={handleDecrementQuantity}>
-					<span className='text-body h4'>-</span>
-				</Button>
-			) : null}
+			<Button
+				style='btn-light mx-1'
+				disabled={minimumProduct}
+				handleClick={handleDecrementQuantity}>
+				<span className='text-body h4'>-</span>
+			</Button>
 
-			<h5 className='text-body'>x{quantity}</h5>
-			{editCart ? (
-				<Button style='btn-light mx-1' handleClick={handleIncrementQuantity}>
-					<span className='text-body h4'>+</span>
-				</Button>
-			) : null}
+			<h5 className='text-body'>X {quantity}</h5>
+
+			<Button style='btn-light mx-1' handleClick={handleIncrementQuantity}>
+				<span className='text-body h4'>+</span>
+			</Button>
 		</div>
 	);
 }
