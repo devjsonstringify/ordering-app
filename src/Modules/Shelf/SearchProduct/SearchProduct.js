9984eab0c294b001d71efb56f7cf0c842f46237c
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-10-12 11:32:50 +0800

    Add: feature/clear button on search bar

--- src/Modules/Shelf/SearchProduct/SearchProduct.js
+++ src/Modules/Shelf/SearchProduct/SearchProduct.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -7,13 +7,26 @@ import style from './index.module.scss';
 import Header from '../../../Components/Header';
 import { getProductQuery } from '../../../Ducks/Features/SearchSlice';
 export default function SearchProduct() {
+	const [isQuery, setIsQuery] = useState('');
+
+	const handleClearBtn = () => {
+		setIsQuery('');
+		dispatch(getProductQuery(''));
+		console.log(isQuery);
+	};
+
+	const handleChangeBar = (e) => {
+		setIsQuery(e.target.value);
+		dispatch(getProductQuery(e.target.value));
+	};
+
 	const dispatch = useDispatch();
 	return (
 		<Header
 			strong='Menu'
 			regular='Category'
 			classes='search flex-column align-items-center mb-4 text-center'>
-			<div className={`input-group input-group-lg my-3 ${style.search}`}>
+			<div className={`input-group input-group-lg my-5 ${style.search}`}>
 				<span className='input-group-text'>
 					<svg
 						width='1em'
@@ -34,11 +47,28 @@ export default function SearchProduct() {
 				</span>
 				<input
 					type='text'
-					className='form-control'
+					className='form-control mr-4'
 					aria-label='Sizing example input'
 					placeholder='Search for food, coffee, etc'
-					onChange={(e) => dispatch(getProductQuery(e.target.value))}
+					value={isQuery}
+					onChange={(e) => handleChangeBar(e)}
 				/>
+				{isQuery.length > 0 && (
+					<p onClick={() => handleClearBtn()}>
+						<svg
+							width='1.5em'
+							height='1.5em'
+							viewBox='0 0 16 16'
+							className='bi bi-x-square-fill'
+							fill='#e7201f'
+							xmlns='http://www.w3.org/2000/svg'>
+							<path
+								fillRule='evenodd'
+								d='M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z'
+							/>
+						</svg>
+					</p>
+				)}
 			</div>
 		</Header>
 	);
