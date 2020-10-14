e4f573306d55b1dfc4bc171d66660f99dc502149
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-10-14 10:01:37 +0800

    Update: conditional rendering of quantity control based on isSubmit value

--- src/Components/FloatCart/Cart/CartProduct/CartProduct.js
+++ src/Components/FloatCart/Cart/CartProduct/CartProduct.js
@@ -18,6 +18,7 @@ export default function Product({ product }) {
 	const { id, price, sku, quantity } = product;
 	const cartId = useSelector((state) => cart.selectById(state, id));
 	const isEdit = useSelector((state) => state.cart.isEdit);
+	const isSubmit = useSelector((state) => state.checkOut.isSubmit);
 	const dispatch = useDispatch();
 	let minimumProduct = cartId.quantity < 2 ? true : false;
 	let calculateAmount = quantity * price;
@@ -72,23 +73,25 @@ export default function Product({ product }) {
 						quantity={quantity}
 						classes='d-flex justify-content-around align-items-center col flex-grow-1 '
 					/>
-					<button
-						type='button'
-						className='btn btn-link p-0 col-1'
-						onClick={() => dispatch(deleteCart(id))}>
-						<svg
-							width='1.25em'
-							height='1.25em'
-							viewBox='0 0 16 16'
-							className='bi bi-trash'
-							fill='#ff2351'
-							xmlns='http://www.w3.org/2000/svg'>
-							<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'></path>
-							<path
-								fillRule='evenodd'
-								d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'></path>
-						</svg>
-					</button>
+					{!isSubmit ? (
+						<button
+							type='button'
+							className='btn btn-link p-0 col-1'
+							onClick={() => dispatch(deleteCart(id))}>
+							<svg
+								width='1.25em'
+								height='1.25em'
+								viewBox='0 0 16 16'
+								className='bi bi-trash'
+								fill='#ff2351'
+								xmlns='http://www.w3.org/2000/svg'>
+								<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z'></path>
+								<path
+									fillRule='evenodd'
+									d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z'></path>
+							</svg>
+						</button>
+					) : null}
 				</div>
 			</div>
 		</div>
