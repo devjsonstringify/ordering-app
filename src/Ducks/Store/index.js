f80957972f9d3f9aca18e4886baf124195a4ac7f
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-10-20 19:29:09 +0800

    Update: add checkout on persist-data

--- src/Ducks/Store/index.js
+++ src/Ducks/Store/index.js
@@ -9,7 +9,7 @@ import rootReducer from '../Reducer/';
 const persistConfig = {
 	key: 'root',
 	storage,
-	whitelist: ['cart'],
+	whitelist: ['cart', 'checkOut'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
