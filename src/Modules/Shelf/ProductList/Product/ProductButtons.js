5443f25be8acc35eb7be4a5f91067003bfb68045
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-10-21 13:18:28 +0800

    Feature: Implement shop notification

--- src/Modules/Shelf/ProductList/Product/ProductButtons.js
+++ src/Modules/Shelf/ProductList/Product/ProductButtons.js
@@ -1,20 +1,41 @@
 import React from 'react';
+import { toast } from 'react-toastify';
+import { isEmpty, isUndefined } from 'lodash';
+import PropTypes from 'prop-types';
 
 // import local file
 import Button from '../../../../Components/Button';
+import Notification from '../../../../Components/Notification';
 
 export default function ProductButtons({
+	item,
 	idAlreadyExists,
 	handleViewProduct,
 	handleAddProduct,
 }) {
+	const notifyDetails = !isEmpty(item) && { ...item };
+	const updateNotify = () => {
+		toast.dismiss();
+		toast('Your item has been updated!', {
+			progressClassName: 'custom_progress',
+		});
+	};
+	const addNotify = () => {
+		toast.dismiss();
+		toast(<Notification detail={notifyDetails} />, {
+			progressClassName: 'custom_progress',
+		});
+	};
 	return (
 		<>
 			{idAlreadyExists ? (
 				<div className='d-flex justify-content-center my-2 products_buttons'>
 					<Button
 						style='d-block w-auto btn-lg m-1 rounded-0'
-						handleClick={handleAddProduct}>
+						handleClick={() => {
+							handleAddProduct();
+							updateNotify();
+						}}>
 						<span className='position-relative'>Add to Cart</span>
 					</Button>
 					<Button
@@ -26,10 +47,26 @@ export default function ProductButtons({
 			) : (
 				<Button
 					style='d-block w-auto btn-lg m-auto my-2 rounded-0'
-					handleClick={handleAddProduct}>
+					handleClick={() => {
+						handleAddProduct();
+						addNotify();
+					}}>
 					<span className='position-relative'>Add to Cart</span>
 				</Button>
 			)}
 		</>
 	);
 }
+ProductButtons.propTypes = {
+	item: PropTypes.shape({
+		id: PropTypes.number,
+		category: PropTypes.string,
+		description: PropTypes.string,
+		name: PropTypes.string,
+		price: PropTypes.number,
+		quantity: PropTypes.number,
+		sku: PropTypes.string,
+	}),
+	handleViewProduct: PropTypes.func,
+	handleAddProduct: PropTypes.func,
+};
