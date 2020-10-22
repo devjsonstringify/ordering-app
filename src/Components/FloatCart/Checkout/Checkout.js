bfbbb8d6866dcf11eb156f5e9936e2c48ddfe045
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-10-23 07:58:26 +0800

    Add:  set state.checkout.notifier to false before proceeding to bills page

--- src/Components/FloatCart/Checkout/Checkout.js
+++ src/Components/FloatCart/Checkout/Checkout.js
@@ -14,6 +14,7 @@ import { isToggle } from '../../../Ducks/Features/SideBar.js';
 import {
 	checkOutIsSubmit,
 	getTransactionId,
+	setBillNotify,
 } from '../../../Ducks/Features/CheckOut.js';
 
 export default function Checkout() {
@@ -35,6 +36,7 @@ export default function Checkout() {
 	}, 0);
 
 	useEffect(() => {
+		dispatch(setBillNotify(false));
 		if (productsOnCart.length > 0) {
 			setCheckoutDetails({
 				...checkoutDetails,
