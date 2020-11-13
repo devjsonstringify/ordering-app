48c8d9cbdc7f2c379c2244b8355b7b740c4df754
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-11-13 20:14:03 +0800

    feat(add copyrights): copyrights

--- src/Components/Footer/CopyRight.js
+++ src/Components/Footer/CopyRight.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import getYear from 'date-fns/getYear';
 
 export default function CopyRight() {
-  return <div>CopyRight</div>;
+  const [copyRight, setCopyRight] = useState('');
+
+  useEffect(() => {
+    setCopyRight(getYear(new Date()));
+  }, []);
+  return (
+    <div className="container-fluid h-25 p-5">
+      <h6 className="text-center">
+        Copyright<span className="h5 mx-2">&copy;</span>
+        {copyRight}. All Right Reserved
+      </h6>
+    </div>
+  );
 }
