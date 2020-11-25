d5f355b1a4343422ff3c9a5116e602b55639f2d6
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-11-25 17:21:12 +0800

    refactor(use signinpage component): clean-code

--- src/Modules/SignIn/SignIn.js
+++ src/Modules/SignIn/SignIn.js
@@ -3,21 +3,12 @@ import React from 'react';
 // import local files
 import './style.scss';
 import Layout from '../../Components/Layout';
-import AccountProfile from '../../Components/AccountProfile';
-import Header from '../../Components/Header';
+import SignInPage from '../../Components/SignInPage';
 
 function SignIn() {
   return (
     <Layout>
-      <div className="sign-in-page container p-5">
-        <Header
-          regular="Please sign-in first with your Google account."
-          strong="In order to proceed,"
-        />
-        <div className="sign-in-button mt-5 border border-5  rounded-pill w-25 bg-primary">
-          <AccountProfile />
-        </div>
-      </div>
+      <SignInPage />
     </Layout>
   );
 }
