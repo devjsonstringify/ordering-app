05a12e3f1eaa81a2e3832c18b779faada9c48642
Glen Mark Cabiladas <dev.json.stringify@gmail.com>
2020-11-25 17:10:51 +0800

    refactor(create sign-in page component): sharable sign-in components

---
+++ src/Components/SignInPage/SignInPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// import local files
+import './style.scss';
+import  AccountProfile from '../AccountProfile';
+import Header from '../Header';
+
+function SignInPage() {
+  return (
+    <div className="sign-in-page container p-5">
+      <Header
+        regular="Please sign-in first with your Google account."
+        strong="In order to proceed,"
+      />
+      <div className="sign-in-button mt-5 border border-5  rounded-pill w-25 bg-primary">
+        <AccountProfile />
+      </div>
+    </div>
+  );
+}
+
+export default SignInPage;
